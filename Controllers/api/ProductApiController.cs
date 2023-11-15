e50ed6c98321e7066ad4e5f9e3266075e27c9268
Cameron P. <cam.peterson@kodirex.space>
2023-11-15 12:21:24 -0600

    Adjust / breakup different components of api.
    This breaks route "Product/Index" as some endpoints have been mildly adjusted to make a bit more sense

---
+++ Controllers/api/ProductApiController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Models;
+
+namespace Northwind.Controllers.api;
+
+[Route("api/product/")]
+public class ProductApiController : Controller
+{
+    private readonly DataContext _dataContext;
+    public ProductApiController(DataContext db) => _dataContext = db;
+    
+    // returns all products (question-mark after url parameter means it's optional)
+    [HttpGet, Route("{discontinued:bool?}")]
+    public IEnumerable<Product> Get(bool discontinued = false) => _dataContext.Products
+        .Where(p => p.Discontinued == discontinued)
+        .OrderBy(p => p.ProductName);
+    
+    // return a specific product
+    [HttpGet, Route("{id:int}")]
+    public Product Get(int id) => _dataContext.Products
+        .FirstOrDefault(p => p.ProductId == id);
+
+    // return a specific product's reviews
+    [HttpGet, Route("{id:int}/reviews")]
+    public IEnumerable<Review> GetReviews(int id) => _dataContext.Reviews
+        .Where(r => r.ProductID == id);
+}
