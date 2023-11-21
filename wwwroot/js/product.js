8a1a93a069258aea19e1987f98c58a41dc5e3704
Kathy Bauer <kbauer17@my.wctc.edu>
2023-11-21 11:10:57 -0600

    formatted Add Review button

--- wwwroot/js/product.js
+++ wwwroot/js/product.js
@@ -16,7 +16,7 @@ $(function () {
                     <td>${response[i].productName}</td>
                     <td class="text-right">${response[i].unitPrice.toFixed(2)}</td>
                     <td class="text-right">${response[i].unitsInStock}</td>
-                    <td><a class="btn bg-primary text-white" href="/Product/Reviews/${response[i].productId}" role="button">Add Review</a></td>
+                    <td class="text-right"><a href="/Product/Reviews/${response[i].productId}" role="button">Add Review</a></td>
               </tr>`;
                     $('#product_rows').append(row);
                 }
